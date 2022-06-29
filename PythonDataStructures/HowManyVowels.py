e12314cdc4792b28e98af1343ff4af30d5b1e459
Enis Toprak Öztürk <enistoprakozturk@gmail.com>
2022-06-29 23:00:26 +0300

    PythonDataStructures

---
+++ PythonDataStructures/HowManyVowels.py
@@ -0,0 +1,7 @@
+str = input()
+syc = 0
+
+for x in str:
+    if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+        syc += 1
+print(syc)
---
+++ PythonDataStructures/LineThemUp.py
@@ -0,0 +1,7 @@
+str = str(input())
+
+syc = 1
+
+for i in str:
+    print(i*syc)
+    syc += 1
